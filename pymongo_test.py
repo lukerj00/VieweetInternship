ff1e0981343305c4da026859fd551837b996f4ad
lukerj00 <66368833+lukerj00@users.noreply.github.com>
2021-08-26 17:29:47 +0100

    Add files via upload

--- pymongo_test.py
+++ pymongo_test.py
@@ -1,2 +1,31 @@
 import pymongo
 import pprint
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client.Vieweet_prototype_01
+models = db.models
+
+#tests
+pprint.pprint(models.find_one())
+pprint.pprint(models.find_one({"classifier":"int_ext_5"}))
+
+for model in models.find():
+    pprint.pprint(model)
+
+test_count = models.find({"accuracy": ["$gte": 0.90]})
+test_count.count_documents()
+
+#retrieve relevant models
+classifier_compare = "int_ext"
+field_compare = "accuracy"
+
+classifiers = models.find({classifier_compare: 1, field_compare: 1})
+
+#classifiers to classifier_list
+
+int_ext_all = []
+for classifier in classifiers #classifier_list:
+    if classifier_compare in classifier:
+        int_ext_all += classifier_compare
+
+pprint(int_ext_all)
